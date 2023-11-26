2899f6c01c542378281cf47a45c9cc822df7042f
25chl2 <chl2@williams.edu>
2023-11-26 21:24:14 +0100

    finished dataframe with genres in larger buckets

--- album_cover_db.py
+++ album_cover_db.py
@@ -10,7 +10,7 @@ from PIL import Image
 def get_genre(genre_list):
     genre_string = str(genre_list)
     genre_split = genre_string.split(",")
-    return genre_split[0]
+    return genre_split[0].replace(' ', '').lower()
 
 # function to download and save image 
 def download_and_save_image(url, filename):
@@ -19,7 +19,7 @@ def download_and_save_image(url, filename):
     img.save(filename)
 
 def label_image(index, row, output_folder):
-    genre = row['genre'].replace(' ', '').lower()
+    genre = row['genre']
     filename = os.path.join(output_folder, genre + '_' + str(index) + '.jpeg')
     return filename
 
@@ -40,8 +40,47 @@ if __name__ == "__main__":
     new_df['id'] = df['id']
     new_df['genre'] = new_genre_column
     new_df['cover'] = df['cover']
+    print('     new_df now ', len(new_df))
     
-    print('done editing union_df.csv')
+    print('done editing union_df.csv, now group genres')
+    
+    # Mapping of genres to buckets
+    genre_buckets = {
+        'rock': ['alternativerock', 'rock', 'classicrock', 'newwave', 'permenantwave', 'adultstandards', 'modernrock', 'albumrock', 'emo', 'altz', 'hardrock', 'punkrock', 'poprock', 'neomellow', 'psychedelicrock', 'softrock', 'bluesrock', 'rootsrock', 'stompandholler', 'countryrock', 'heartlandrock', 'glamrock', 'sleazerock', 'rockandroll', 'dancerock', 'britishinvasion', 'grunge', 'acidrock', 'pianorock', 'garagerock', 'freakbeat', 'merseybeat', 'rockenespanol', 'mpb', 'rockquebecois', 'rockbaiano', 'eggpunk', 'trashrock', 'rocknacional', 'rockabilly', 'rockabillyenespanol', 'novacanco', 'swissalternativerock', 'flamencofusion', 'italianpoprock', 'rhythmrock', 'grunge pop', 'grungerevival', 'newzealandrock', 'countryrock'],
+        'pop': ['pop', 'dancepop', 'electropop', 'indiepop', 'newwavepop', 'bubblegumpop', 'sophistipop', 'brillbuildingpop', 'teenpop', 'europop', 'lapop', 'popelectronico', 'poprap', 'popworship', 'poppunk', 'popsoul', 'classicukpop', 'popr&b', 'popemo', 'ccm', 'popreggaeton', 'poplgbtq+brasileira'],
+        'metal': ['metal', 'numetal', 'alternativemetal', 'powermetal', 'epicore', 'glammetal', 'deathmetal', 'melodicmetal', 'speedmetal', 'thrashmetal', 'oldschoolthrashmetal', 'neoclassicalmetal', 'paganmetal', 'vikingmetal', 'tolkienmetal', 'technicaldeathmetal', 'brutaldeathmetal', 'groovemetal', 'nümetal', 'metalcore', 'melodicdeathmetal', 'doommetal', 'gothicmetal', 'industialmetal', 'djent', 'folkmetal', 'avantgardemetal', 'symphonicmetal', 'progressivemetal', 'sludgemetal', 'stonermetal', 'dronemetal', 'postmetal', 'atmosphericblackmetal', 'blackmetal', 'depressiveblackmetal'],
+        'hiphop': ['hiphop', 'rap', 'poprap', 'undergroundhiphop', 'southernhiphop', 'trap', 'melodicrap', 'raprock', 'rapmetal', 'oldschoolhiphop', 'hardorehiphop', 'gangsterrap', 'rapconscient', 'abstracthiphop', 'boom bap', 'jazzrap', 'eastcoasthiphop', 'westcoasthiphop', 'dirtysouthrap', 'choppedandscewed', 'memphishiphop', 'neworleansbounce', 'hyphy', 'crunk', 'phonk', 'cloudrap', 'soundcloudrap', 'mumblerap', 'afropop', 'hiplife', 'gengetone', 'asanteakanhighlife', 'ghanaianhiplife', 'hippop', 'hawaiianhiphop', 'melodicrap'],
+        'electronic': ['electronic','tronica','indieelectropop','edm', 'chillstep', 'electronica', 'electro', 'electroclash', 'electropop', 'electropowerpop', 'indietronica', 'electronicrock', 'electronic', 'house', 'techno', 'trance', 'hardstyle', 'hardcore', 'jungle', 'drumandbass', 'dubstep', 'brostep', 'glitchhop', 'industrial', 'darkelectro', 'ebm', 'futurepop', 'synthpop', 'synthwave', 'vaporwave', 'hyperpop', 'artifical', 'breakbeat', 'bigbeat', 'tekno', 'gabber', 'speedcore', 'happyhardcore', 'hardstyle', 'UKhardcore', 'makina', 'techstep', 'darksidejungle', 'darkstep', 'neurofunk', 'techno', 'minimaltechno', 'nrghardcore', 'breakcore', 'rhythmgame', 'videogamemusic', 'chiptune', 'bitpop', 'intelligentdancemusic', 'braindance', 'experimental', 'lofi', 'lo-fi', 'complextro', 'speedrun', 'bigroom'],
+        'dance': ['dance', 'dancepop', 'popdance', 'house', 'tropicalhouse', 'floatinghouse', 'shuffle', 'basshouse', 'clubhouse', 'slaphouse', 'groovehouse', 'funkyhouse', 'discohouse', 'psytrance', 'techhouse', 'electrohouse', 'futurehouse', 'bounce', 'crunk', 'bass', 'tribalhouse', 'highenergy', 'eurobeat', 'hyperbeat', 'speedcore', 'speedgarage', '2stepgarage', 'futuregarage', 'garagerock'],
+        'funk': ['funk', 'pfunk', 'gogo', 'boogie', 'electrofunk', 'modernfunk'],
+        'folk': ['folk', 'indiefolk', 'freakfolk', 'folkrock', 'americana', 'roots', 'blue'],
+        'classical': ['classical'],
+        'country': ['country'],
+        'r&b': ['r&b', 'soul', 'neosoul', 'quietstorm', 'contemporaryr&b', 'alternativer&b'],
+        'indie': ['indie', 'indierock', 'indiepop', 'indiepoptimism', 'indier&b', 'indiefolk', 'indieelectronica', 'singersongwriter'],
+        'reggae' : ['reggae'],
+    }
+
+    # Default bucket for genres not in any specified bucket
+    default_bucket = 'other'
+
+    # Update genre column based on buckets
+    new_df['genre_no_bucket'] = new_df['genre']
+    new_df['genre'] = new_df['genre'].apply(lambda x: next((bucket for bucket, genres in genre_buckets.items() if any(keyword in x.lower() for keyword in genres)), default_bucket))
+
+    # Print counts of genres in each bucket
+    for bucket, genres in genre_buckets.items():
+        count = new_df[new_df['genre'].isin(genres)].shape[0]
+        print(f'{bucket}: {count}')
+
+    # Count of genres in the default bucket
+    count_other = new_df[new_df['genre'] == default_bucket].shape[0]
+    print(f'{default_bucket}: {count_other}')
+
+    # Save the updated DataFrame to a new CSV file
+    new_df.to_csv('genre_df.csv', index=False)
+    print('Done updating genres, saved in genre_df.csv')
+
 
     # Set output folder path
     output_folder = './album_covers'
